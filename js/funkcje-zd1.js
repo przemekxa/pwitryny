a9787b9a7ccf75759c4556c0205cd232fe0c5c54
Przemek <przemek19986@gmail.com>
2017-09-15 17:16:10 +0200

    Funkcje - dokończone (z ZD)

---
+++ js/funkcje-zd1.js
@@ -0,0 +1,29 @@
+//document.write("W");
+
+//Napisać skrypt który oblicza obwód, pole oraz objętość stożka. Dane user podaje z klawiatury, wykorzystaj obiekt Math. Użytkownik wybiera za pomocą pola radio co chce obliczyć. Dane wprowadza w formularzu. Wynik wyświetl w bloku.
+
+var wynik = document.getElementById("wynik");
+
+function licz(f) {
+    var promien = f.promien.value;
+    var wysokosc = f.wysokosc.value;
+    var typ = f.typ.value;
+
+    if(promien != "" && wysokosc != "" && typ != "") {
+        promien = parseFloat(promien);
+        wysokosc = parseFloat(wysokosc);
+        switch (typ) {
+            case "obwod":
+                wynik.innerHTML = "Obwód: " + 2 * promien * Math.PI;
+                break;
+            case "pole":
+                wynik.innerHTML = "Pole: " + promien * promien * Math.PI;
+                break;
+            case "objetosc":
+                wynik.innerHTML = "Objętość: " + promien * promien * Math.PI * wysokosc;
+                break;
+        }
+    } else {
+        alert("Wprowadź wszystkie dane!")
+    }
+}
--- js/funkcje.js
+++ js/funkcje.js
@@ -24,3 +24,23 @@ function licz() {
     var elWyswietl = document.getElementById("wyswietl");
     elWyswietl.innerHTML = "Pole prostokąta: " + pole + "cm<sup>2</sup>";
 }
+
+function poleObjetosc(szer, dl, wys) {
+    var pole = szer * dl;
+    var obj = pole * wys;
+    return [pole, obj];
+}
+
+console.log(poleObjetosc(3, 4, 5));
+
+var pole = poleObjetosc(3, 4, 10)[0];
+var obj = poleObjetosc(3, 4, 19)[1];
+console.log(pole);
+console.log(obj);
+
+
+//var x = prompt("Wybierz: 0 - pole, 1 - objętość:");
+//console.log(poleObjetosc(1,2,3)[x]);
+
+
+console.log(Math.PI);
