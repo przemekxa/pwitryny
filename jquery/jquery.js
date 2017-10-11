e57bb7de1bdf881eb672a8678c14b3c5c53675e5
przemek_szkola <>
2017-10-11 09:04:17 +0200

    jQuery - cd

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -2,8 +2,35 @@ jQuery(document).ready(function() {
 
     $("#tekst").click(function() {
 
-        $("#lorem").toggle();
+        //$("#lorem").toggle();
+        //$("#lorem").toggle("slow");
+        //$("#lorem").toggle("normal");
+        //$("#lorem").toggle("fast");
+        $("#lorem").toggle(500);
 
-    })
 
-})
+        $("div").css("background", "yellow");
+
+        $(".tekst").css("background", "#86d0f2");
+
+        $("#id1").css("background", "red");
+        $("#id2").css("color", "white");
+
+        $("#id1").css({"background":"white", "color":"red"});
+
+        $("p").css("font-family", "Arial, Verdana");
+        $("p").css({"color":"red", "font-size": "120%"});
+
+    });
+
+
+    $("#zmien_kolor").click(function() {
+
+        $("p").css("color", "lime");
+
+    });
+
+
+});
+
+
